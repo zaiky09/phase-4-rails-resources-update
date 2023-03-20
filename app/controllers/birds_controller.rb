b84396b3734c0c1f7fa9f3b51e0879aaf2b4a7f9
zaiky09 <zamilmozamil@outlook.com>
2023-03-20 18:25:25 +0300

    done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -22,10 +22,31 @@ class BirdsController < ApplicationController
     end
   end
 
+  #PATCH/PUT /birds/:id
+  def update 
+    bird = Bird.find_by(id: params[:id])
+    if Bird.update(bird_params)
+      render json: bird, status: :accepted
+    else
+      render json: {error: "Bird not found"}, status: :not_found
+    end
+  end
+
+  #Increment of likes
+  def increment_likes
+    bird = Bird.findby(id: params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird, status: :accepted
+    else
+      render json: {error: "Bird not found"}, status: :not_found
+    end
+  end
+
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
 
 end
